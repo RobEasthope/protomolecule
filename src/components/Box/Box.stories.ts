46bb23d96c4c699ae9ed466d06c346b2d98b96df
Rob Easthope <hello@robeasthope.com>
2024-05-24 14:55:03 +0100

    Clean up comments

--- src/components/Box/Box.stories.ts
+++ src/components/Box/Box.stories.ts
@@ -1,34 +1,28 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Box } from "./Box";
+import type { Meta, StoryObj } from '@storybook/react';
+import { Box } from './Box';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Base UI/Box",
+  title: 'Base UI/Box',
   component: Box,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
+    layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  tags: ['autodocs'],
 } satisfies Meta<typeof Box>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    as: "section",
-    children: "Box",
+    as: 'section',
+    children: 'Box',
   },
 };
 
 export const NoChildren: Story = {
   args: {
-    as: "div",
+    as: 'div',
     children: null,
   },
 };
