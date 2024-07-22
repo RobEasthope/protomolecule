e0a9457eb9cbfd5c8c9c97d5bd39a6798bd50856
Rob Easthope <hello@robeasthope.com>
2024-07-22 11:49:14 +0100

    Create Container.stories.ts

---
+++ src/components/Container/Container.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Container } from "./Container";
+
+const meta = {
+  title: "Base UI/Container",
+  component: Container,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Container>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    as: "section",
+    children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+};
+
+export const NoChildren: Story = {
+  args: {
+    as: "div",
+    children: null,
+  },
+};
