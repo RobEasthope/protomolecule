fc7cd4ff103db4426376679bca4b4ed84a5b0bd1
Rob Easthope <hello@robeasthope.com>
2024-07-22 11:45:05 +0100

    Strip back Box component to absolute basics

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,48 +1,21 @@
-import { cva, type VariantProps } from 'class-variance-authority';
-import { createElement, forwardRef } from 'react';
+import { createElement, forwardRef } from "react";
 
-import { areThereAnyStyles, cn } from '@/utils/tailwind';
+import { areThereAnyStyles, cn } from "@/utils/tailwind";
 
-const boxVariants = cva(
-  // Base styles
-  null,
-  {
-    variants: {
-      breakout: {
-        true: 'max-w-screen relative left-[50%] right-[50%] -ml-[50vw] -mr-[50vw] w-screen',
-      },
-      maxWidth: {
-        none: '',
-        auto: 'max-w-auto',
-        small: 'max-w-lg',
-        text: 'max-w-prose',
-        medium: 'max-w-5xl',
-        large: 'max-w-7xl',
-        full: 'w-screen',
-      },
-    },
-    defaultVariants: {
-      maxWidth: null,
-    },
-  },
-);
-
-export interface BoxProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof boxVariants> {
+export interface BoxProps {
   as: string;
+  className?: string;
+  children?: React.ReactNode;
 }
 
 const Box = forwardRef<HTMLDivElement, BoxProps>(
-  ({ as = 'div', className, breakout, maxWidth, children, ...props }, ref) => {
+  ({ as = "div", className, children, ...props }, ref) => {
     if (!children) return null;
 
     return createElement(
       as,
       {
-        className: areThereAnyStyles(
-          cn(boxVariants({ breakout, maxWidth }), className),
-        ),
+        className: areThereAnyStyles(cn(className)),
         ref,
         ...props,
       },
@@ -51,6 +24,6 @@ const Box = forwardRef<HTMLDivElement, BoxProps>(
   },
 );
 
-Box.displayName = 'Box';
+Box.displayName = "Box";
 
-export { Box, boxVariants };
+export { Box };
