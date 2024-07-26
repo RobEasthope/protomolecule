09ebb47e5222fef763dde904cb7a13517fd13c9c
Rob Easthope <hello@robeasthope.com>
2024-07-26 11:31:08 +0100

    Merge containerVariants back into main file

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,7 +2,32 @@ import { type VariantProps } from "class-variance-authority";
 import { createElement } from "react";
 
 import { areThereAnyStyles, cn } from "@/utils/tailwind";
-import { containerVariants } from "@/components/Container/Container.variants";
+
+import { cva } from "class-variance-authority";
+
+const containerVariants = cva(
+  // Base styles
+  null,
+  {
+    variants: {
+      breakout: {
+        true: "max-w-screen relative left-[50%] right-[50%] -ml-[50vw] -mr-[50vw] w-screen",
+      },
+      maxWidth: {
+        none: "",
+        auto: "max-w-auto",
+        small: "max-w-lg",
+        text: "max-w-prose",
+        medium: "max-w-5xl",
+        large: "max-w-7xl",
+        full: "w-screen",
+      },
+    },
+    defaultVariants: {
+      maxWidth: null,
+    },
+  },
+);
 
 export interface ContainerProps
   extends React.HTMLAttributes<HTMLDivElement>,
