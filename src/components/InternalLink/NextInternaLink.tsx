17174c6979713e9c3a12417b279fb7a6955ef0e7
Rob Easthope <hello@robeasthope.com>
2024-07-26 10:02:26 +0100

    Clean up next link code

--- src/components/InternalLink/NextInternaLink.tsx
+++ src/components/InternalLink/NextInternaLink.tsx
@@ -1,37 +1,17 @@
 import Link from "next/link";
-import { SanityReference } from "ui-pkg/types/sanity-schema";
-import { HOME_PAGE_SLUG } from "ui-pkg/pages/Page/constants/HOME_PAGE_SLUG";
-import { PageProps } from "ui-pkg/pages/Page/Page";
-import { NewsArticleProps } from "ui-pkg/pages/NewsArticle/NewsArticle";
-
-// Schema props
-export type InternalLinkWithTitleSchemaProps = {
-  _type: "InternalLinkWithTitle";
-  _key: string;
-  internalUID: SanityReference<PageProps["page"] | NewsArticleProps["page"]>;
-  to?: PageProps["page"] | NewsArticleProps["page"];
-  title: string;
-};
-
-export type InternalLinkSchemaProps = {
-  _type: "InternalLinkSansTitle";
-  _key: string;
-  internalUID: SanityReference<PageProps["page"] | NewsArticleProps["page"]>;
-  to?: PageProps["page"] | NewsArticleProps["page"];
-};
+import React from "react";
 
 // Component props
 export type InternalLinkProps = React.HTMLAttributes<HTMLAnchorElement> & {
   href: string | null;
   className?: string;
-  children: any;
+  children: React.ReactNode;
 };
 
 export const InternalLink = ({
   href,
   children,
   className,
-  onClick,
   ...rest
 }: InternalLinkProps) => {
   if (!href && !children) {
@@ -44,11 +24,10 @@ export const InternalLink = ({
 
   return (
     <Link
-      href={href === HOME_PAGE_SLUG ? "/" : `/${href}`}
+      href={href === "home" ? "/" : `/${href}`}
       className={className}
       role="link"
       tabIndex={0}
-      onClick={onClick}
       {...rest}
     >
       {children || null}
