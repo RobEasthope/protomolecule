e559b70508b514c6828af440991e93e6e0a30075
Rob Easthope <hello@robeasthope.com>
2025-09-01 21:18:24 +0100

    fix: add ESLint configurations to packages

---
+++ packages/eslint-config/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  }
+}
