3ddc1abcb7b2506080ed1c52c4c26735578a4b62
Rob Easthope <hello@robeasthope.com>
2025-09-01 21:20:06 +0100

    fix: resolve TypeScript issues in UI package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -7,8 +7,8 @@ export { NextInternalLink } from './components/InternalLink/NextInternalLink';
 export { ReactRouterLink } from './components/InternalLink/ReactRouterLink';
 export { OmniLink } from './components/OmniLink/OmniLink';
 export { Prose } from './components/Prose/Prose';
-export { BasicProse } from './components/Prose/components/BasicProse/BasicProse';
-export { FullProse } from './components/Prose/components/FullProse/FullProse';
+export { BasicProseComponents } from './components/Prose/components/BasicProse/BasicProse';
+export { FullProseComponents } from './components/Prose/components/FullProse/FullProse';
 export { Type } from './components/Type/Type';
 
 // Utilities
@@ -16,15 +16,15 @@ export { cn } from './utils/tailwind';
 export { checkForStylingClasses } from './utils/checkForStylingClasses';
 export { stylesCheck } from './utils/stylesCheck';
 
-// Sanity schemas
-export { EmailLinkSchema } from './components/EmailLink/EmailLink.schema';
-export { EmailLinkWithTitleSchema } from './components/EmailLink/EmailLinkWithTitle.schema';
-export { ExternalLinkSchema } from './components/ExternalLink/ExternalLink.schema';
-export { ExternalLinkWithTitleSchema } from './components/ExternalLink/ExternalLinkWithTitle.schema';
-export { InternalLinkSchema } from './components/InternalLink/InternalLink.schema';
-export { InternalLinkWithTitleSchema } from './components/InternalLink/InternalLinkWithTitle.schema';
-export { BasicProseSchema } from './components/Prose/components/BasicProse/BasicProse.schema';
-export { FullProseSchema } from './components/Prose/components/FullProse/FullProse.schema';
+// Sanity schemas (default exports)
+export { default as EmailLinkSchema } from './components/EmailLink/EmailLink.schema';
+export { default as EmailLinkWithTitleSchema } from './components/EmailLink/EmailLinkWithTitle.schema';
+export { default as ExternalLinkSchema } from './components/ExternalLink/ExternalLink.schema';
+export { default as ExternalLinkWithTitleSchema } from './components/ExternalLink/ExternalLinkWithTitle.schema';
+export { default as InternalLinkSchema } from './components/InternalLink/InternalLink.schema';
+export { default as InternalLinkWithTitleSchema } from './components/InternalLink/InternalLinkWithTitle.schema';
+export { default as BasicProseSchema } from './components/Prose/components/BasicProse/BasicProse.schema';
+export { default as FullProseSchema } from './components/Prose/components/FullProse/FullProse.schema';
 
 // Constants
 export { LINKABLE_DOC_TYPES } from './components/InternalLink/LINKABLE_DOC_TYPES';
