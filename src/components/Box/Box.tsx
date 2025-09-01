60d4f8e8ed128b9a70ee40ddf50f0155033068cf
Rob Easthope <hello@robeasthope.com>
2025-09-01 13:55:20 +0100

    refactor: remove forwardRef from Box component for React 19 compatibility

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,4 +1,4 @@
-import { createElement, forwardRef } from "react";
+import { createElement } from "react";
 
 import { cn } from "@/utils/tailwind";
 import { checkForStylingClasses } from "@/utils/checkForStylingClasses";
@@ -7,23 +7,22 @@ export interface BoxProps {
   as: string;
   className?: string;
   children?: React.ReactNode;
+  ref?: React.Ref<HTMLDivElement>;
 }
 
-const Box = forwardRef<HTMLDivElement, BoxProps>(
-  ({ as = "div", className, children, ...props }, ref) => {
-    if (!children) return null;
+const Box = ({ as = "div", className, children, ref, ...props }: BoxProps) => {
+  if (!children) return null;
 
-    return createElement(
-      as,
-      {
-        className: checkForStylingClasses(cn(className)),
-        ref,
-        ...props,
-      },
-      children,
-    );
-  },
-);
+  return createElement(
+    as,
+    {
+      className: checkForStylingClasses(cn(className)),
+      ref,
+      ...props,
+    },
+    children,
+  );
+};
 
 Box.displayName = "Box";
 
