b83b3dab9af425fee674ffc8efc85e372ae2db85
Rob Easthope <hello@robeasthope.com>
2025-09-01 19:25:55 +0100

    Refactor ESLint configuration syntax in protomolecule.eslint.ts
    - Updated the export syntax for the TypeScript ESLint configuration to improve readability and maintainability.
    - Ensured consistent formatting by removing unnecessary brackets.

--- src/eslint/protomolecule.eslint.ts
+++ src/eslint/protomolecule.eslint.ts
@@ -7,10 +7,10 @@ import {
   import auto from "eslint-config-canonical/auto";
   import tseslint from "typescript-eslint";
   
-  export default tseslint.config([
+  export default tseslint.config(
     baseConfig,
     ...auto,
     packageJsonConfig,
     storybookConfig,
     customRules,
-  ]) as unknown;
+  ) as unknown;
