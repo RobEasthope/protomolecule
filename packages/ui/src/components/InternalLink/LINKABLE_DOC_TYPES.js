f6ddfca4f8308b5743ebc9fc3d141fe38bda0122
Rob Easthope <hello@robeasthope.com>
2025-09-01 21:21:20 +0100

    fix: remove duplicate JS files and fix test setup

--- packages/ui/src/components/InternalLink/LINKABLE_DOC_TYPES.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LINKABLE_DOC_TYPES = void 0;
-exports.LINKABLE_DOC_TYPES = [{ type: "Page" }];
--- packages/ui/src/utils/checkForStylingClasses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkForStylingClasses = void 0;
-function checkForStylingClasses(styles) {
-    if (!styles)
-        return undefined;
-    return styles;
-}
-exports.checkForStylingClasses = checkForStylingClasses;
