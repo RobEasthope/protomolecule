5794b092c11f410751a26d9b3d0924f24ab130a9
Rob Easthope <hello@robeasthope.com>
2025-09-04 13:07:22 +0100

    Refactor ESLint configuration to streamline rule exports
    - Updated the main ESLint source file to export rules from individual modules for better organization
    - Improved maintainability and clarity of the ESLint configuration structure

--- packages/eslint-config/protomolecule.eslint.ts
+++ packages/eslint-config/protomolecule.eslint.ts
@@ -1,10 +1,8 @@
-import {
-  ignoredFolders,
-  customRules,
-  packageJsonRules,
-  storybookRules,
-  typescriptOverrideRules,
-} from "./protomolecule.eslintSource";
+export { ignoredFolders } from "./rules/ignored-folders";
+export { customRules } from "./rules/custom-rules";
+export { packageJsonRules } from "./rules/package-json-rules";
+export { storybookRules } from "./rules/storybook-rules";
+export { typescriptOverrideRules } from "./rules/typescript-override-rules";
 import eslintConfigCanonicalAuto from "eslint-config-canonical/auto";
 import tseslint from "typescript-eslint";
 
