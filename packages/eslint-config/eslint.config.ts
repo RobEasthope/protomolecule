4434a6f97d5d427f152779f8176dd89e1903f79b
Rob Easthope <hello@robeasthope.com>
2025-09-04 15:20:48 +0100

    Import preferences rule into ESLint configuration for improved linting consistency

--- packages/eslint-config/eslint.config.ts
+++ packages/eslint-config/eslint.config.ts
@@ -1,6 +1,6 @@
-import { preferences } from "./rules/preferences";
 import { ignoredFileAndFolders } from "./rules/ignoredFileAndFolders";
 import { packageJson } from "./rules/packageJson";
+import { preferences } from "./rules/preferences";
 import { storybook } from "./rules/storybook";
 import { typescriptOverrides } from "./rules/typescriptOverrides";
 import eslintConfigCanonicalAuto from "eslint-config-canonical/auto";
