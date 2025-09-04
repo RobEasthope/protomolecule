2677d4bbd85d829538550f3130b5b62079bdc1db
Rob Easthope <hello@robeasthope.com>
2025-09-04 14:27:00 +0100

    Refactor ESLint configuration imports for consistency
    - Updated import statements in eslint.config.ts to use consistent naming conventions for custom rules
    - Improved maintainability by aligning rule imports with their respective file names

--- packages/eslint-config/eslint.config.ts
+++ packages/eslint-config/eslint.config.ts
@@ -1,16 +1,16 @@
-import { customRules } from "./rules/custom";
+import { custom } from "./rules/custom";
 import { ignoredFolders } from "./rules/ignoredFolders";
-import { packageJsonRules } from "./rules/package-json-rules";
-import { storybookRules } from "./rules/storybook";
-import { typescriptOverrideRules } from "./rules/typescriptOverrides";
+import { packageJson } from "./rules/packageJson";
+import { storybook } from "./rules/storybook";
+import { typescriptOverrides } from "./rules/typescriptOverrides";
 import eslintConfigCanonicalAuto from "eslint-config-canonical/auto";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
   ignoredFolders,
   ...eslintConfigCanonicalAuto,
-  packageJsonRules,
-  storybookRules,
-  typescriptOverrideRules,
-  customRules,
+  packageJson,
+  storybook,
+  typescriptOverrides,
+  custom,
 ]) as unknown;
