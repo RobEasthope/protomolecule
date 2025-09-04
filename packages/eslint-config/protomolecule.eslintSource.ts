fa83032aea8dfab9c492fc411b1e8463fe0dfdce
Rob Easthope <hello@robeasthope.com>
2025-09-04 13:11:51 +0100

    Remove deprecated ESLint source file and its exports
    - Deleted the protomolecule.eslintSource.ts file as part of the ongoing cleanup of the ESLint configuration
    - Streamlined the ESLint setup by removing unused exports, enhancing maintainability

--- packages/eslint-config/protomolecule.eslintSource.ts
+++
@@ -1,5 +0,0 @@
-export { ignoredFolders } from "./rules/ignored-folders";
-export { customRules } from "./rules/custom-rules";
-export { packageJsonRules } from "./rules/package-json-rules";
-export { storybookRules } from "./rules/storybook-rules";
-export { typescriptOverrideRules } from "./rules/typescript-override-rules";
