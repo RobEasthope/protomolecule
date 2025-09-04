24b94f8ad33c35fbf8e82b8c7a7f6b107a23550c
Rob Easthope <hello@robeasthope.com>
2025-09-04 12:23:52 +0100

    Enhance ESLint configuration by restructuring export format
    - Combined recommended and stylistic configurations from typescript-eslint
    - Improved organization of ESLint configuration for better maintainability

--- packages/eslint-config/protomolecule.eslint.ts
+++ packages/eslint-config/protomolecule.eslint.ts
@@ -7,10 +7,12 @@ import {
 import auto from "eslint-config-canonical/auto";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  baseConfig,
+export default [
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
   ...auto,
+  baseConfig,
   packageJsonConfig,
   storybookConfig,
-  customRules
-) as unknown;
+  customRules,
+] as const;
