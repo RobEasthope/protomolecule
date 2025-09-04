3480fabea0f6e62c25ba091a47a03c4336d13042
Rob Easthope <hello@robeasthope.com>
2025-09-04 16:51:25 +0100

    Refactor ESLint configuration in UI package to use Protomolecule preset
    - Replaced custom ESLint rules with the Protomolecule ESLint configuration for improved consistency and maintainability.
    - Removed deprecated imports and streamlined the configuration to a single export.

--- packages/ui/eslint.config.ts
+++ packages/ui/eslint.config.ts
@@ -1,15 +1,3 @@
-import { ignoredFileAndFolders } from "./rules/ignoredFileAndFolders";
-import { packageJson } from "./rules/packageJson";
-import { preferences } from "./rules/preferences";
-import { storybook } from "./rules/storybook";
-import { typescriptOverrides } from "./rules/typescriptOverrides";
-import eslintConfigCanonicalAuto from "eslint-config-canonical/auto";
+import protomolecule from "@protomolecule/eslint-config";
 
-export default [
-  ignoredFileAndFolders,
-  ...eslintConfigCanonicalAuto,
-  packageJson,
-  storybook,
-  typescriptOverrides,
-  preferences,
-];
+export default [...protomolecule];
