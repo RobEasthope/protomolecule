5e646ab9267c51c45efcc8829dc9cc592891a954
Rob Easthope <hello@robeasthope.com>
2025-09-04 14:51:25 +0100

    Refactor ESLint configuration export format
    - Changed the export statement in eslint.config.ts to an array format for improved clarity and simplicity
    - Enhanced maintainability by streamlining the configuration structure

--- packages/eslint-config/eslint.config.ts
+++ packages/eslint-config/eslint.config.ts
@@ -4,13 +4,12 @@ import { packageJson } from "./rules/packageJson";
 import { storybook } from "./rules/storybook";
 import { typescriptOverrides } from "./rules/typescriptOverrides";
 import eslintConfigCanonicalAuto from "eslint-config-canonical/auto";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config([
+export default [
   ignoredFolders,
   ...eslintConfigCanonicalAuto,
   packageJson,
   storybook,
   typescriptOverrides,
   custom,
-]) as unknown;
+];
