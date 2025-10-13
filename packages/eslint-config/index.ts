6ff486ec2ae60eb47c19771b709f2e675c482dd9
Rob Easthope <hello@robeasthope.com>
2025-10-13 14:51:15 +0100

    refactor(eslint-config): remove unused plugin re-exports
    Remove re-exports of ESLint plugins and parsers from index.ts.
    These exports are no longer needed since:
    
    1. Plugins are now regular dependencies (not peerDependencies)
    2. Consumers don't import plugins separately - they use the config directly
    3. Plugin resolution works automatically with flat config
    
    The only internal import kept is pluginImportX, which is used
    for the import/import-x plugin aliasing in the config array.
    
    This simplifies the public API and reduces bundle size.

--- packages/eslint-config/index.ts
+++ packages/eslint-config/index.ts
@@ -11,26 +11,6 @@ import { typescriptOverrides } from "./rules/typescriptOverrides";
 import eslintConfigCanonicalAuto from "eslint-config-canonical/auto";
 import pluginImportX from "eslint-plugin-import-x";
 
-// Re-export Astro parser for proper resolution in monorepo workspaces
-export { default as astroParser } from "astro-eslint-parser";
-// Re-export plugins for workspace consumers
-// This fixes plugin resolution when eslint-config is installed at monorepo root
-// but ESLint is run from workspace subdirectories
-export { default as pluginAstro } from "eslint-plugin-astro";
-export { default as pluginImportX } from "eslint-plugin-import-x";
-export { default as pluginJsdoc } from "eslint-plugin-jsdoc";
-// @ts-expect-error - No types available for this plugin
-export { default as pluginJsxA11y } from "eslint-plugin-jsx-a11y";
-export { default as pluginN } from "eslint-plugin-n";
-export { default as pluginPrettier } from "eslint-plugin-prettier";
-// @ts-expect-error - No types available for this plugin
-export { default as pluginPromise } from "eslint-plugin-promise";
-export { default as pluginReact } from "eslint-plugin-react";
-export { default as pluginReactHooks } from "eslint-plugin-react-hooks";
-export { default as pluginRegexp } from "eslint-plugin-regexp";
-export { default as pluginUnicorn } from "eslint-plugin-unicorn";
-export * as typescriptEslint from "typescript-eslint";
-
 const config: any[] = [
   // Plugin aliasing for eslint-config-canonical compatibility
   // Canonical config references "import" but package is "eslint-plugin-import-x"
